eb9fa66d10df8ae1fa2e5af7b99823bcdb313a33
Kevin Lin <kevin.lin20000@gmail.com>
2026-08-03 01:15:48 -0700

    Open drawer for stable marker support

--- examples/run_putmarker_skill_program.py
+++ examples/run_putmarker_skill_program.py
@@ -68,7 +68,7 @@ def _parser() -> argparse.Namespace:
     parser.add_argument("--handle-pull-joint-extension", type=float, default=-0.05)
     parser.add_argument("--drawer-placement-q-m", type=float, default=0.055)
     parser.add_argument("--drawer-pull-extra-m", type=float, default=0.010)
-    parser.add_argument("--rigid-handle-pull-m", type=float, default=0.10)
+    parser.add_argument("--rigid-handle-pull-m", type=float, default=0.15)
     parser.add_argument("--handle-pull-vertical-offset-m", type=float, default=-0.040)
     parser.add_argument(
         "--integrate-left-ik",
